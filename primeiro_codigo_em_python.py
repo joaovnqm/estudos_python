0dc398ba05ffc68c253afdc6aa9c441cf0b398d0
João Victor Macêdo <joaovictornqm@gmail.com>
2025-11-29 10:52:14 -0300

    Atualização do Primeiro Código em Python

--- primeiro_codigo_em_python.py
+++ primeiro_codigo_em_python.py
@@ -1,3 +1,16 @@
-# Primeiro código em Python
+# Primeiro código em Python.
 print("Olá Mundo!")
 print("Bem-vindo ao Python.")
+
+# Por convenção, constantes são escritas em maiúsculas.
+NOME = "João"
+
+# Variáveis podem ser escritas em minúsculas. E é utilizado o padrão snake_case.
+idade_usuario = 21
+
+# Salvando variáveis em uma mesma linha.
+estado_residencia, cidade_residencia = "Pernambuco", "Recife"
+print(f"Olá, meu nome é {NOME} e eu tenho {str(idade_usuario)} anos. Eu moro e {cidade_residencia}, {estado_residencia}.")
+
+# Exibindo o tipo da variável do NOME.
+print(type(NOME))

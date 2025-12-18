072d2ce39341244f40621a5d3cd676ae40a07414
João Victor Macêdo <joaovictornqm@gmail.com>
2025-12-17 21:35:05 -0300

    Adicionando exercício para comparação de valores

--- exercicios.py
+++ exercicios.py
@@ -28,3 +28,19 @@ imc = peso / altura ** 2
 
 # A seguinte string é uma f-string, pode incluir variáveis dentro dela através do uso das chaves.
 print(f"O indivíduo de nome: {nome}, de altura: {altura} e de peso: {peso}, tem um IMC de: {imc:.2f}")
+
+# Outro exercício:
+primeiro_valor = input("Digite o primeiro valor: ")
+segundo_valor = input("Digite o segundo valor: ")
+
+if primeiro_valor > segundo_valor:
+    print(f"O primeiro valor = {primeiro_valor} é maior do que o segundo valor = {segundo_valor}.")
+
+elif segundo_valor > primeiro_valor:
+    print(f"O segundo valor = {segundo_valor} é maior do que o primeiro valor = {primeiro_valor}.")
+
+elif primeiro_valor == segundo_valor:
+    print(f"O primeiro valor: {primeiro_valor} e o segundo valor: {segundo_valor} são iguais.")
+
+else:
+    print("Um ou os dois valores inseridos são inválidos.")

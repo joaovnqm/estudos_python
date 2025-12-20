4fc4f7dd213beec47ace94f6a6f3980e28f0eb49
João Victor Macêdo <joaovictornqm@gmail.com>
2025-12-19 23:48:35 -0300

    Adicionando exercícios sobre condicionais e tratamento de exceções em Python.

--- exercicios_2.py
+++ exercicios_2.py
@@ -5,3 +5,38 @@ try:
 except:
     print("O que você digitou não é um número e contém algum caractere não esperado como letras ou espaços. " \
     "Por favor, analise e tente novamente.")
+
+# Exercício 1 da Aula 55 do Curso do Luiz Otávio Miranda.
+numero_inteiro = input("Por favor, digite um número inteiro para saber se ele é par ou ímpar: ")
+try:
+    if int(numero_inteiro) % 2 == 0:
+        print(f"O número: {numero_inteiro} é par.")
+
+    elif int(numero_inteiro) % 2 != 0:
+        print(f"O número: {numero_inteiro} é ímpar.")
+        
+except:
+    print("O valor digitado não é um valor inteiro. Por favor, tente novamente.")
+
+# Exercício 2 da Aula 55 do Curso do Luiz Otávio Miranda.
+hora_dia = int(input("Por favor, informe a hora do dia: "))
+try:
+    if hora_dia < 12:
+        print("Bom dia.")
+    elif hora_dia < 18:
+        print("Boa tarde.")
+    elif hora_dia < 24:
+        print("Boa noite.")
+    else:
+        print("Hora inválida.")
+except:
+    print("O valor digitado não é um valor inteiro. Por favor, tente novamente.")
+
+# Exercício 3 da Aula 55 do Curso do Luiz Otávio Miranda.
+nome_usuario = input("Por favor, escreva o seu nome: ")
+if len(nome_usuario) <= 4:
+    print("O seu nome é pequeno.")
+elif len(nome_usuario) <= 6:
+    print("O seu nome é normal.")
+else:
+    print("O seu nome é muito grande.")

e3a4544d92449db32699d9e1ed4e6cbb15b536a8
João Victor Macêdo <joaovictornqm@gmail.com>
2025-12-23 15:28:20 -0300

    Adicionando a introdução dos estudos de empacotamento e de desempacotamento.

--- exercicios/exercicios_5.py
+++ exercicios/exercicios_5.py
@@ -2,20 +2,25 @@
 # Exercício: Exibindo os índices da lista.
 
 # Exibindo o índice através de um método rudimentar.
-lista = ["João Victor", "Rafael", "Anna", "Marcos"]
+lista_1 = ["João Victor", "Rafael", "Anna", "Marcos"]
 indice = 0
 
-for nome in lista:
+for nome in lista_1:
     print(indice, " - ", nome)
     indice += 1
 
 # Exibindo o índice através de um método melhorado.
-indices = range(len(lista))
+indices = range(len(lista_1))
 
 for i in indices:
-    print(i, " - ", lista[i])
+    print(i, " - ", lista_1[i])
     
 # Exibindo o índice através de um método eficiente.
 
-for novo_indice, nome in enumerate(lista):
+for novo_indice, nome in enumerate(lista_1):
     print(novo_indice, " - ", nome)
+
+# Introdução ao empacotamento e ao desempacotamento.
+lista_2 = ["João Victor", "Rafael", "Anna", "Marcos"]
+nome_1, nome_2, nome_3, nome_4 = lista_2
+print(nome_1)

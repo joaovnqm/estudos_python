32be0352ce658355ab93d62555357b5efd5bc317
João Victor Macêdo <joaovictornqm@gmail.com>
2025-12-30 16:12:14 -0300

    Adicionando função para duplicar, triplicar e quadruplicar números utilizando closures em "exercicios_7.py"

--- exercicios/exercicios_7.py
+++ exercicios/exercicios_7.py
@@ -15,3 +15,18 @@ def par_ou_impar(numero):
 numeros = 2, 3, 4, 5
 resultado = multiplicar(*numeros)
 print(f"Os números fornecidos são: {numeros}. O resultado de sua multiplicação é: {resultado}, que é um número {par_ou_impar(resultado)}")
+
+# Realizando outro exercício:
+# Função que duplica, triplica e quadruplica um número.
+def criar_multiplicador(multiplicador):
+    def multiplicar_numero(numero):
+        return numero * multiplicador
+    return multiplicar_numero
+
+duplicar = criar_multiplicador(2)
+triplicar = criar_multiplicador(3)
+quadruplicar = criar_multiplicador(4)
+
+print(duplicar(5))
+print(triplicar(5))
+print(quadruplicar(5))

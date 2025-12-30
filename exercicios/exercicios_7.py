f182591ab8fbb3ec3425bf654b005cdc52ea43d5
João Victor Macêdo <joaovictornqm@gmail.com>
2025-12-30 13:37:26 -0300

    Adicionando função para multiplicar argumentos e verificar se o resultado é par ou ímpar em "exercicios_7.py"

--- exercicios/exercicios_7.py
+++ exercicios/exercicios_7.py
@@ -1,2 +1,18 @@
 # Resolvendo exercícios de Python
-# Exercício de funções com retorno
+# Exercício de funções com retorno, e argumentos não nomeados. Multiplicando argumentos e dizendo se o resultado é par ou ímpar.
+def multiplicar(*args):
+    resultado = 1
+    for numero in args:
+        resultado *= numero
+    return resultado
+
+def par_ou_impar(numero):
+    if numero % 2 == 0:
+        return "par"
+    else:
+        return "ímpar"
+
+# Testando a função:
+numeros = 2, 3, 4, 5
+resultado = multiplicar(*numeros)
+print(f"Os números fornecidos são: {numeros}. O resultado de sua multiplicação é: {resultado}, que é um número {par_ou_impar(resultado)}")

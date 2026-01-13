d49d6212a3713270e84a795da45954401246824b
João Victor Macêdo <joaovictornqm@gmail.com>
2026-01-12 22:30:52 -0300

    Adicionando exercício utilizando generator para produzir números pares de 1 a 50 no arquivo "exercicios_10.py"

--- exercicios/exercicios_10.py
+++ exercicios/exercicios_10.py
@@ -20,3 +20,15 @@ print(set)
 # A letra maiúscula e o acento são considerados caracteres diferentes de acordo com a tabela Unicode,
 # por isso, aparecem dois "i"s, um com o acento agudo e outro sem acento. Poderia acontecer o mesmo caso
 # existisse uma letra "f" minúscula na frase.
+
+# Exercício utilizando generator
+# Criar um generator que produza os números pares de 1 a 50.
+def gerar_numeros_pares(limite):
+    for numero in range(1, limite + 1):
+        if numero % 2 == 0:
+            yield numero
+
+pares_generator = gerar_numeros_pares(50)
+for par in pares_generator:
+    print(par)
+    input("Pressione Enter para ver o próximo número par...")

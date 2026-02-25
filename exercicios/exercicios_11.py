1e42c7e0e194ab20b24a3fc32a528ab2d094a1df
João Victor Macêdo <joaovictornqm@gmail.com>
2026-02-25 11:52:20 -0300

    Adicionando o exercício de closures ao arquivo: exercicios_11.py

--- exercicios/exercicios_11.py
+++ exercicios/exercicios_11.py
@@ -23,3 +23,23 @@ print(*novos_produtos, sep='\n', end='\n \n')
 
 novos_produtos.sort(key=lambda item: item['preco'])
 print(*novos_produtos, sep='\n', end='\n \n')
+
+# Exercício - Adiando execução de funções
+def soma(x, y):
+    return x + y
+
+
+def multiplica(x, y):
+    return x * y
+
+
+def criar_funcao(funcao, x):
+    def funcao_interna(y):
+        return funcao(x, y)
+    return funcao_interna
+
+
+soma_com_cinco = criar_funcao(soma, 5)
+multiplica_por_dez = criar_funcao(multiplica, 10)
+
+print(soma_com_cinco(10), multiplica_por_dez(10))

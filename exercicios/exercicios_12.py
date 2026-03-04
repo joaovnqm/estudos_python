3bedc32c0c9410ebd5b975dc4631dd3b69af049d
João Victor Macêdo <joaovictornqm@gmail.com>
2026-03-04 12:55:26 -0300

    Refatorando o código do arquivo: exercicios_12.py

--- exercicios/exercicios_12.py
+++ exercicios/exercicios_12.py
@@ -2,7 +2,9 @@
 cidades = ["Recife", "São Paulo", "Porto Alegre", "Manaus"]
 estados = ["PE", "SP", "RS", "AM", "MT"]
 def zipper(lista1, lista2):
-    intervalo = min(lista1, lista2)
+    intervalo = min(len(lista1), len(lista2))
     return [(lista1[i], lista2[i]) for i in range(intervalo)]
 
 print(zipper(cidades, estados))
+# Refatorando o código adicionando a função built-in Zip.
+print(list(zip(cidades, estados)))

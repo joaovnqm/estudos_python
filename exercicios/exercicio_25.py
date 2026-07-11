5c9284020b191cfc0edc3307a5c16bcf702f8930
João Victor Macêdo <joaovictornqm@gmail.com>
2026-07-11 14:23:37 -0300

    Atualiza exercício 25 para incluir métodos de propriedades.

--- exercicios/exercicio_25.py
+++ exercicios/exercicio_25.py
@@ -1,4 +1,4 @@
-# Exercício 25 - Métodos de Classe, Factory e Métodos Estáticos
+# Exercício 25 - Métodos de Classe, Factory, Métodos Estáticos e Properties
 # Criando uma classe de um sistema hipotético de cadastro
 class Usuario:
     def __init__(self, nome, username, email):
@@ -13,6 +13,11 @@ class Usuario:
     @staticmethod
     def bem_vindo():
         return "Bem-vindo ao sistema!"
+    
+    # Usando uma property como um getter
+    @property
+    def nome(self):
+        return self.nome
 
 usuario_1 = Usuario("João", "joao.macedo97", "email@exemplo.com")
 usuario_2 = Usuario.usuario_anonimo("OAnonimo", "anonimo@exemplo.com")
